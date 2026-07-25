b4517fd8ad635504a5faac754fe3203ec7b2488c
Fern el Jaguar <fer875nandosm@gmail.com>
2026-07-25 17:17:50 -0600

    Realizado los ejercicios de tipos de datos

---
+++ Día 2/PracticaConFloats.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------
+# Este programa declara una variable llamada "num_decimal" y
+# le asigna un valor de tipo decimal (float). Posteriormente,
+# utiliza la función type() para identificar el tipo de dato
+# de la variable y mostrarlo en pantalla.
+# ------------------------------------------------------------
+
+num_decimal = 3.14  # Puedes elegir cualquier número decimal.
+
+print(type(num_decimal))
---
+++ Día 2/PracticaConIntegers.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------
+# Este programa declara una variable llamada "num_entero" y
+# le asigna un valor de tipo entero (int). A continuación,
+# utiliza la función type() para identificar el tipo de dato
+# de la variable y mostrarlo en pantalla.
+# ------------------------------------------------------------
+
+num_entero = 42  # Puedes elegir cualquier número entero.
+
+print(type(num_entero))
