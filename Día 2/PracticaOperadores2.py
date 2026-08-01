93c75472620ffb18366fb2e18ed5114b32e4c714
Fern el Jaguar <fer875nandosm@gmail.com>
2026-08-01 16:23:35 -0600

    Programa para mostrar en pantalla el cociente de una división

---
+++ Día 2/PracticaOperadores2.py
@@ -0,0 +1,7 @@
+# ------------------------------------------------------------
+# Este programa calcula el módulo (es decir, el resto) de la
+# división entre los números 456 y 33 utilizando el operador
+# %. Finalmente, muestra el resultado obtenido en pantalla.
+# ------------------------------------------------------------
+
+print(456 % 33)
