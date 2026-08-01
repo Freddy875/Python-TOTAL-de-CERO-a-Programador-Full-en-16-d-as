0c5cf2c618df2372713d0af006d9a09da2600558
Fern el Jaguar <fer875nandosm@gmail.com>
2026-08-01 16:20:54 -0600

    Programa para calcular y mostrar en pantalla la raíz cuadrada de un número

---
+++ Día 2/PracticaOperadores1.py
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------
+# Este programa calcula el cociente de una división entera
+# utilizando el operador // (división al piso). Este
+# operador devuelve únicamente la parte entera del resultado,
+# descartando la parte decimal.
+# ------------------------------------------------------------
+
+print(874 // 27)
