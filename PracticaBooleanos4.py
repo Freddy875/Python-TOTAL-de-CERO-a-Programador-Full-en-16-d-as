46b83ca5dd63374435c9340a278174a19a4aff83
Fern el Jaguar <fer875nandosm@gmail.com>
2026-09-05 18:11:01 -0600

    Finalmente mostrar el reusltado de la primer operación en solo dos decimales

--- PracticaBooleanos4.py
+++ PracticaBooleanos4.py
@@ -22,13 +22,15 @@ print(f"El resultado de la operación 2 es: {operacion_2}")
 # Comparar los resultados
 # Se comparan los valores numéricos para indicar cuál es
 # mayor, cuál es menor o si ambos son iguales.
+# En el mensaje final, el resultado de la operación 1 se
+# muestra con únicamente 2 decimales.
 # ------------------------------------------------------------
 
 if operacion_1 > operacion_2:
-    print(f"{operacion_1} es mayor que {operacion_2}")
+    print(f"{operacion_1:.2f} es mayor que {operacion_2}")
 
 elif operacion_1 < operacion_2:
-    print(f"{operacion_1} es menor que {operacion_2}")
+    print(f"{operacion_1:.2f} es menor que {operacion_2}")
 
 else:
-    print(f"{operacion_1} es igual a {operacion_2}")
+    print(f"{operacion_1:.2f} es igual a {operacion_2}")
