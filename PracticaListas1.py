bd69f0cde9c0aaa354b4b194c49d0d4b7e666c33
Fern el Jaguar <fer875nandosm@gmail.com>
2026-09-05 18:31:32 -0600

    Elimine toda la basura

--- PracticaListas1.py
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------------------------
-# Este programa crea una lista con cinco elementos de
-# diferentes tipos de datos y posteriormente muestra su
-# contenido en pantalla.
-# ------------------------------------------------------------
-
-mi_lista = ["Python", 42, True, 3.14, "Lista mixta"]
-
-print(mi_lista)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Python-TOTAL-de-CERO-a-Programador-Full-en-16-d-as
-Aprendiendo sobre python 
