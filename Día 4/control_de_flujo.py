b298e9f1f95af5b5e31d022e1bec2fc865a01d0a
Fern el Jaguar <fer875nandosm@gmail.com>
2026-09-15 22:14:31 -0600

    Apreiendo sobre el control de flujos

---
+++ Día 4/control_de_flujo.py
@@ -0,0 +1,42 @@
+if True:
+    print("Condición verdadera")
+else:
+    print("Condición falsa")
+
+if 10 > 9:
+    print("Es corrrecto 10 es mayor que 9")
+
+### Condición que nunca se cumple
+if 10 > 99:
+    print("Es corrrecto 10 es mayor que 9")
+
+x = 3
+
+if x > 100:
+    print("Es correcto")
+else:
+    print("Es incorrecto")
+
+### Determinar el tipo de masocota
+mascota = "gato"
+
+if mascota == "perro":
+    print("Tienes un perro")
+elif mascota == "canario":
+    print("Tines un canario")
+elif mascota == "hamster":
+    print("Tienes un hamster")
+elif mascota == "gato":
+    print("Tienes un gato")
+elif mascota == "pez":
+    print("Tienes un pez")
+elif mascota == "conejo":
+    print("Tienes un conejo")
+else:
+    print("No se que tipo de mascota tienes")
+
+### If anidado
+
+
+
+
