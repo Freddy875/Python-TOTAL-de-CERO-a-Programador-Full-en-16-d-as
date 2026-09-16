ac07529eecf6692d01bdb28f6ab90ed4f90fdb13
Fern el Jaguar <fer875nandosm@gmail.com>
2026-09-16 16:35:56 -0600

    Aprendiendo sobre match

---
+++ Día 4/Match.py
@@ -0,0 +1,23 @@
+### Hacer coicidir un número con serie con una marca
+
+serie = "N-02"
+
+if serie == "N-01":
+    print("Samsung")
+elif serie == "N-02":
+    print("Apple")
+elif serie == "N-03":
+    print("Xiaomi")
+elif serie == "N-04":
+    print("Motorola")
+elif serie == "N-05":
+    print("Huawei")
+elif serie == "N-06":
+    print("LG")
+elif serie == "N-07":
+    print("Sony")
+elif serie == "N-08":
+    print("Nokia")
+else:
+    print("Serie no reconocida")
+
